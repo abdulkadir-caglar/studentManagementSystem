d5e2581d05629e68876d1d92ddca2692db2fc269
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-12 17:50:45 +0300

    main class

--- src/studentManagementSystem/Application.java
+++ src/studentManagementSystem/Application.java
@@ -1,5 +1,21 @@
 package studentManagementSystem;
 
+import java.awt.EventQueue;
+
+import studentManagementSystem.view.MainFrame;
+
 public class Application {
 	
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					MainFrame frame = new MainFrame();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
 }
