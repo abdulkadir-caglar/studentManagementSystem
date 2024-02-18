3c2abd77a16073e9ec7bee2f6e0b667ba5266be0
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-18 22:59:45 +0300

    creating course internal frame

---
+++ src/studentManagementSystem/view/admin/internalFrame/course/CourseInternalFrame.java
@@ -0,0 +1,12 @@
+package studentManagementSystem.view.admin.internalFrame.course;
+
+import javax.swing.JInternalFrame;
+
+public class CourseInternalFrame extends JInternalFrame {
+
+	public CourseInternalFrame() {
+		setBounds(100, 100, 450, 300);
+
+	}
+
+}
