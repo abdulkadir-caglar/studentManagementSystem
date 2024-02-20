3cffd09cd14e659610feffdea5429d4ac724c4f5
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-20 22:49:57 +0300

    getter methods for admin navbar buttons and creating instance

--- src/studentManagementSystem/view/admin/navbar/AdminNavBar.java
+++ src/studentManagementSystem/view/admin/navbar/AdminNavBar.java
@@ -4,11 +4,22 @@ import java.awt.Font;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
+import studentManagementSystem.viewController.AdminNavbarController;
+
 import java.awt.Dimension;
 
 public class AdminNavBar extends JPanel {
+	private static AdminNavBar instance;
+	
 	private JButton btnStudent;
 	private JButton btnCourse;
+	
+	public static AdminNavBar getInstance() {
+		if(instance == null) {
+			instance = new AdminNavBar();
+		}return instance;
+	}
 	
 	public AdminNavBar() {
 		this.setBounds(100,100,800,50);
@@ -18,11 +29,21 @@ public class AdminNavBar extends JPanel {
 		btnStudent = new JButton("Students");
 		btnStudent.setPreferredSize(new Dimension(100, 40));
 		btnStudent.setFont(font);
+		btnStudent.addActionListener(new AdminNavbarController());
 		this.add(btnStudent);
 		
 		btnCourse = new JButton("Courses");
 		btnCourse.setPreferredSize(new Dimension(100, 40));
 		btnCourse.setFont(font);
+		btnCourse.addActionListener(new AdminNavbarController());
 		this.add(btnCourse);
+	}
+	
+	public JButton getBtnStudent() {
+		return this.btnStudent;
+	}
+	
+	public JButton getBtnCourse() {
+		return this.btnCourse;
 	}
 }
