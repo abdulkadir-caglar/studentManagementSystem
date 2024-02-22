3a5d8e6fa160e834539a8b335ab6ff8a453511e9
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-22 18:28:46 +0300

    creating get queries for student

--- src/studentManagementSystem/repository/student/StudentQueries.java
+++ src/studentManagementSystem/repository/student/StudentQueries.java
@@ -3,7 +3,19 @@ package studentManagementSystem.repository.student;
 public class StudentQueries {
 	private String saveStudent = "INSERT INTO student (student_name, student_last_name, email, password) VALUES (?, ?, ?, ?)";
 	
+	private String findStudentByLogin = "SELECT * FROM student WHERE email=? AND password=?";
+	
+	private String findStudentByEmail= "SELECT * FROM student WHERE email=?";
+	
 	public String getSaveStudent() {
 		return this.saveStudent;
+	}
+
+	public String getFindStudentByLogin() {
+		return findStudentByLogin;
+	}
+	
+	public String getFindStudentByEmail() {
+		return findStudentByEmail;
 	}
 }
