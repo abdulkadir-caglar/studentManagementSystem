2bad6b92b9fa5c394a1d6388da80931b75442838
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-22 18:29:56 +0300

    updating dto object for student with encryptor object

--- src/studentManagementSystem/dto/studentDto/StudentDto.java
+++ src/studentManagementSystem/dto/studentDto/StudentDto.java
@@ -1,10 +1,12 @@
 package studentManagementSystem.dto.studentDto;
 
+import studentManagementSystem.encryptor.Encryptor;
 import studentManagementSystem.model.student.Student;
 import studentManagementSystem.view.register.RegisterFrame;
 
 public class StudentDto {
 	private Student student;
+	private Encryptor encryptor = new Encryptor();
 	
 	public Student setStudent() {
 		student = new Student();
@@ -12,7 +14,7 @@ public class StudentDto {
 		student.setFirstName(RegisterFrame.getInstance().getNameTF().getText());
 		student.setLastName(RegisterFrame.getInstance().getSurnameTF().getText());
 		student.setEmail(RegisterFrame.getInstance().getEmailTF().getText());
-		student.setPassword(RegisterFrame.getInstance().getPasswordTF().getText());
+		student.setPassword(encryptor.encryptString(RegisterFrame.getInstance().getPasswordTF().getText()));
 	
 		return student;
 	}
