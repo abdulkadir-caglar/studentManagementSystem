a3317c4c8f5a17e93c946e48218d07f1597518ab
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-22 18:28:12 +0300

    creating get methods for student by mail and password

--- src/studentManagementSystem/controller/student/StudentController.java
+++ src/studentManagementSystem/controller/student/StudentController.java
@@ -1,15 +1,22 @@
 package studentManagementSystem.controller.student;
 
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import studentManagementSystem.dto.passwordDto.PasswordConverter;
+import studentManagementSystem.encryptor.Encryptor;
 import studentManagementSystem.model.student.Student;
 import studentManagementSystem.repository.dbConfig.DatabaseConfig;
 import studentManagementSystem.repository.student.StudentQueries;
+import studentManagementSystem.view.main.MainFrame;
+import studentManagementSystem.view.register.RegisterFrame;
 
 public class StudentController {
 	private DatabaseConfig db = new DatabaseConfig();
 	private StudentQueries queries = new StudentQueries();
+	private Encryptor encryptor = new Encryptor();
+	private PasswordConverter pc = new PasswordConverter();
 	
 	public void saveStudent(Student student) {
 		try {
@@ -23,6 +30,29 @@ public class StudentController {
 			
 		} catch (SQLException e) {
 			e.printStackTrace();
+		}
+	}
+	
+	public ResultSet getStudentByLogin() {
+		try {
+			PreparedStatement ps = db.connect().prepareStatement(queries.getFindStudentByLogin());
+			ps.setString(1, MainFrame.getInstance().getEmailTF().getText());
+			ps.setString(2, encryptor.encryptString(pc.convertPassword()));
+			return ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public ResultSet getStudentByEmail() {
+		try {
+			PreparedStatement ps = db.connect().prepareStatement(queries.getFindStudentByEmail());
+			ps.setString(1, RegisterFrame.getInstance().getEmailTF().getText());
+			return ps.executeQuery();
+		} catch (SQLException e) {
+			e.printStackTrace();
+			return null;
 		}
 	}
 }
