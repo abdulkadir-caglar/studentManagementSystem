157726d53c39993b2f9abfade9e4a182bf073b25
abdulkadir-caglar <110019810+abdulkadir-caglar@users.noreply.github.com>
2024-02-26 21:31:41 +0300

    main controller fixed for check text fields

--- src/studentManagementSystem/viewController/MainFrameController.java
+++ src/studentManagementSystem/viewController/MainFrameController.java
@@ -5,19 +5,20 @@ import java.awt.event.ActionListener;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+import javax.swing.JOptionPane;
+
 import studentManagementSystem.controller.student.StudentController;
 import studentManagementSystem.dto.passwordDto.PasswordConverter;
 import studentManagementSystem.session.SessionInfo;
 import studentManagementSystem.view.admin.AdminFrame;
 import studentManagementSystem.view.main.MainFrame;
 import studentManagementSystem.view.register.RegisterFrame;
+import studentManagementSystem.viewModel.LoginCheck;
 
 public class MainFrameController implements ActionListener{
 	private PasswordConverter pc = new PasswordConverter();
 	private StudentController studentController = new StudentController();
 	private SessionInfo session = new SessionInfo();
-	
-	private int userId;
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
@@ -30,16 +31,19 @@ public class MainFrameController implements ActionListener{
 			try {
 				ResultSet rs = studentController.getStudentByLogin();
 				
-				if(MainFrame.getInstance().getEmailTF().getText().equals("admin") && pc.convertPassword().equals("admin")) {
-					MainFrame.getInstance().dispose();
-					AdminFrame.getInstance().setVisible(true);
-				}else if(rs.next()) {
-					session.setUserId(rs.getInt("id"));
-					session.setUserName(rs.getString("student_name"));
-					session.setUserSurname(rs.getString("student_last_name"));
-					session.setUserEmail(rs.getString("email"));
-				}
-					
+				if(LoginCheck.getInstance().loginCheck()) {
+					if(MainFrame.getInstance().getEmailTF().getText().equals("admin") && pc.convertPassword().equals("admin")) {
+						MainFrame.getInstance().dispose();
+						AdminFrame.getInstance().setVisible(true);
+					}else if(rs.next()) {
+						session.setUserId(rs.getInt("id"));
+						session.setUserName(rs.getString("student_name"));
+						session.setUserSurname(rs.getString("student_last_name"));
+						session.setUserEmail(rs.getString("email"));
+					}else {
+						JOptionPane.showMessageDialog(null, "User not found.");
+					}
+				}	
 			} catch (SQLException exp) {
 				exp.printStackTrace();
 			}
